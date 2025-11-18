50653417e0ee6805f850e8fe298161d123bb820c
abhishek90900 <mukherjeeabhishek613@gmail.com>
2025-11-18 20:54:13 +0530

    api url update in frontend

--- NAKSHATRA/src/pages/Homepage.jsx
+++ NAKSHATRA/src/pages/Homepage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import './HomePage.css'; 
+import './home.css'; 
 import BookCard from '../components/BookCard'; 
 import heroImage from '../assets/book hone.jpg'; 
 
